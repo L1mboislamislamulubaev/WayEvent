4a453b116cfc1b3fe9e3be745bcaba93430b7053
L1mboislamislamulubaev <islamulubaev2008@gmail.com>
2023-11-05 13:16:53 +0300

    disin

--- src/Components/header/Nav/Nav.jsx
+++ src/Components/header/Nav/Nav.jsx
@@ -20,12 +20,40 @@ const Nav = () => {
           <div className={`${cl.rating}`}>
               <p className={`${cl.rating_text}`}>Оценка полезности</p>
               <div className={`${cl.rating_img}`}> <img src="./rating.png" alt="" /></div>
+              <div className={`${cl.navigation}`}>
+                  <p className={`${cl.map}`}>как добраться?</p>
+                <img src="./map.png" alt="" />
+              </div>
+              <div className={`${cl.textButt}`}>4.87</div>
             </div>
         </div>
       <div className={`${cl.titleDown}`}></div>
     </div>
-    <div className={`${cl.background_time}`}></div>
-    <div className={`${cl.background_reviews}`}></div>
+    <div className={`${cl.background_time}`}>
+    <div className={`${cl.title_block_Up}`}>
+          <div className={`${cl.titleUp}`}>VkFest</div>
+          <div className={`${cl.rating}`}>
+              <p className={`${cl.rating_text}`}>Продолжительность</p>
+             
+              <div className={`${cl.navigation}`}>
+                  <p className={`${cl.map, cl.time}`}>90 мин.</p>
+               
+              </div>
+              
+            </div>
+        </div>
+    </div>
+    <div className={`${cl.background_reviews}`}>
+    <div className={`${cl.title_block_Up}`}>
+          <div className={`${cl.titleUp}`}>Количество отзывов</div>
+          <div className={`${cl.rating}`}>
+              
+                  <p className={`${cl.rev_v}`}>113</p>
+              
+              
+            </div>
+        </div>
+    </div>
     </div>
     </div>
   )
