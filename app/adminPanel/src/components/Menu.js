a42613c47bb19a72cafa4e0256f55cc68061d48c
L1mboislamislamulubaev <149874439+L1mboislamislamulubaev@users.noreply.github.com>
2023-11-05 14:49:31 +0300

    dfgdfgd

---
+++ app/adminPanel/src/components/Menu.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
+import { Menu } from 'antd'
+import { HomeOutlined, HistoryOutlined, TeamOutlined, UserOutlined, SolutionOutlined } from '@ant-design/icons'
+
+const MenuComponent = () => {
+  const { pathname } = window.location
+  const items = [
+    {
+      key: '/',
+      label: (
+        <MenuLink to={`/`}>
+          <HomeOutlined style={{ marginRight: 8 }} />
+          Главная
+        </MenuLink>
+      ),
+    },
+    {
+      key: '/events',
+      label: (
+        <MenuLink to={`/events`}>
+          <SolutionOutlined style={{ marginRight: 8 }} />
+          Мероприятия
+        </MenuLink>
+      ),
+    },
+   
+    {
+      key: '/users',
+      label: (
+        <MenuLink to={`/users`}>
+          <TeamOutlined style={{ marginRight: 8 }} />
+          Пользователи
+        </MenuLink>
+      ),
+    },
+    {
+      key: '/history',
+      label: (
+        <MenuLink to={`/history`}>
+          <HistoryOutlined style={{ marginRight: 8 }} />
+          История
+        </MenuLink>
+      ),
+    },
+  ]
+  return <Menu theme='dark' mode='inline' defaultSelectedKeys={[pathname]} items={items} />
+}
+
+const MenuLink = styled(NavLink)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  a {
+    color: white;
+  }
+`
+
+export default MenuComponent
